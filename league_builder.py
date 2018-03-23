8c7c7632ae57f4d9c3405f7dce27904fe73179e4
Khem Myrick <khemmeta@gmail.com>
2018-03-23 12:36:02 -0700

    Added leave_builder python file.
    I'm adding the first sketch of my soccer league coordinator python file.  (I'm writing the file in Treehouse Workspaces).

---
+++ league_builder.py
@@ -0,0 +1,43 @@
+import csv
+
+if __name__ == "__main__":
+    sharks = []
+    dragons = []
+    raptors = []
+    advanced = []
+    recruits = []
+    with open('soccer_players.csv', newline='') as csvroster:
+        rows = list(csv.DictReader(csvroster, delimiter=','))
+        headings = rows[0]
+        roster = rows[1:]
+        for row in rows:
+            if row['Soccer Experience'] == 'YES':
+                advanced.append(row)
+            else:
+                recruits.append(row)
+            # print(row['Soccer Experience'])
+        print(advanced)        
+            
+    # 1. TRYHARD MODE:
+    # Set league_type, U6, U8, U10, U12 or U13.
+    # Set default value of league_type to U8 for this project.
+    # For a U8 class league_type, have lbp calc how many experienced players can be distributed to the available number of teams of the specific size., 
+    # 2. ACTUAL ASSIGNMENT:
+    # In the list of teams include the team name on one line, followed by a separate line for each player. Include the player's name, whether the player has experience playing soccer, and the player's guardian names. Separate each bit of player information by a comma. For example, the text file might start something like this:
+     
+# Sharks
+# Frank Jones, YES, Jim and Jan Jones
+# Sarah Palmer, YES, Robin and Sari Washington
+# Joe Smith, NO, Bob and Jamie Smith 
+
+
+
+    # Generate lists of experienced vs inexperienced players.
+    # Use loop to evenly pop experienced players into 3 team lists.
+    # Use another loop to pop remainder of players into same 3 teams.
+    
+    # with open('teams.txt', 'a') as teamrosters:
+        # fieldnames = ['get_these_from_roster']
+        # teamrosters.write(csvfile, fieldnames=fieldnames)
+# else:
+    # print("Due to tariffs, this file cannot be imported.")
