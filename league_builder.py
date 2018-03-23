35cd160cb291e6beffbf2e09495954f35e324f02
Khem Myrick <khemmeta@gmail.com>
2018-03-23 13:29:37 -0700

    Updated league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -7,21 +7,35 @@ if __name__ == "__main__":
     advanced = []
     recruits = []
     with open('soccer_players.csv', newline='') as csvroster:
+        # Get players from csv file.
         rows = list(csv.DictReader(csvroster, delimiter=','))
         headings = rows[0]
         roster = rows[1:]
         for row in rows:
+            # Seperate experienced players from beginners.
             if row['Soccer Experience'] == 'YES':
                 advanced.append(row)
             else:
                 recruits.append(row)
-            # print(row['Soccer Experience'])
-        print(advanced)        
-            
+
+        while advanced or recruits:
+            """ Distribute players to actual teams. """
+            if advanced != []:
+                for row in advanced:
+                    sharks.append(advanced.pop())
+                    dragons.append(advanced.pop())
+                    raptors.append(advanced.pop())
+            elif recruits != []:
+                for row in recruits:
+                    sharks.append(recruits.pop())
+                    dragons.append(recruits.pop())
+                    raptors.append(recruits.pop())
+        print("Number of Sharks: {}".format(len(sharks)))
     # 1. TRYHARD MODE:
     # Set league_type, U6, U8, U10, U12 or U13.
     # Set default value of league_type to U8 for this project.
     # For a U8 class league_type, have lbp calc how many experienced players can be distributed to the available number of teams of the specific size., 
+
     # 2. ACTUAL ASSIGNMENT:
     # In the list of teams include the team name on one line, followed by a separate line for each player. Include the player's name, whether the player has experience playing soccer, and the player's guardian names. Separate each bit of player information by a comma. For example, the text file might start something like this:
      
