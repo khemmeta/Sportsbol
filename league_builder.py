c952293bea517f1a38259fd585fc7055bd6e1959
Khem Myrick <khemmeta@gmail.com>
2018-03-26 21:59:05 -0700

    Updated league_builder.py
    This version of league_builder.py appears to meet project specs.  It takes info from the csv file, arranges it into teams, which it writes to a teams.txt file.
    
    On the next version, I'll have it write letters to the parents.

--- league_builder.py
+++ league_builder.py
@@ -18,58 +18,70 @@ def unpacker(pre_rosters):
         pre_rosters.pop(0)
         return row_list
 
+def string_list(team):
+    """ Prepare team strings."""
+    player_string = ''
+    for player in team:        
+        del player[1]
+        player_string += str(player)
+        player_string = player_string.replace('[', '')
+        player_string = player_string.replace(']', '')
+        player_string = player_string.replace("'", "")
+        player_string += '\n'        
+    player_string += '\n'
+    return player_string
+        
+
 if __name__ == "__main__":
 
 
-    with open('soccer_players.csv', newline='') as csvroster, open('example.txt', 'w') as teamrosters:
-        # Get players from csv file.
+    with open('soccer_players.csv', newline='') as csvroster: 
+        # Get players from csv file and put into roster list.
         roster = list(csv.DictReader(csvroster, delimiter=','))
-        # These ordered maps are a pain in my brain.
-        for player in roster:
-            # Seperate experienced players from beginners.
-            if player['Soccer Experience'] == 'YES':
-                advanced.append(player)
-            else:
-                recruits.append(player)
-                # This part of my code does what I want it to do!
-
-        while advanced or recruits:
-            """ Distribute players to actual teams. """
-            if advanced != []:
-                sharks.append(unpacker(advanced))
-                dragons.append(unpacker(advanced))
-                raptors.append(unpacker(advanced))
-            elif recruits != []:
-                sharks.append(unpacker(recruits))
-                dragons.append(unpacker(recruits))
-                raptors.append(unpacker(recruits))
-            # Everything's lists now.  No dicts.  Almost there?
-
+        # We should be done with the csv file.  Deindenting steps below.
         
-        
-        if len(sharks) == len(raptors) and len(raptors) == len(dragons):
-            print("Time to write txt files!")
-            # write(this that and the other thing)
+    for player in roster:
+        # Seperate experienced players from beginners.
+        if player['Soccer Experience'] == 'YES':
+            advanced.append(player)
         else:
-            print("Error: Uneven teams.")
-    # 1. TRYHARD MODE: (let's just write the extra credit letters and ditch the league types and heights business.  Classes will likely come into play in project 2 anyway. 
-    # EC: Create welcome letter text files to each of the 18 students indicating which team they're on etc.
-    # 2. ACTUAL ASSIGNMENT:
-    # In the list of teams include the team name on one line, followed by a separate line for each player. Include the player's name, whether the player has experience playing soccer, and the player's guardian names. Separate each bit of player information by a comma. For example, the text file might start something like this:
-     
-# Sharks
-# Frank Jones, YES, Jim and Jan Jones
-# Sarah Palmer, YES, Robin and Sari Washington
-# Joe Smith, NO, Bob and Jamie Smith 
+            recruits.append(player)
+            # This part of my code does what I want it to do!
+
+    while advanced or recruits:
+        """ Distribute players to actual teams. """
+        if advanced != []:
+            sharks.append(unpacker(advanced))
+            dragons.append(unpacker(advanced))
+            raptors.append(unpacker(advanced))
+        elif recruits != []:
+            sharks.append(unpacker(recruits))
+            dragons.append(unpacker(recruits))
+            raptors.append(unpacker(recruits))
 
 
+    if len(sharks) == len(raptors) and len(raptors) == len(dragons):
+        with open('teams.txt', 'a') as teamrosters:
+            
+            teamrosters.write("Sharks \n")
+            teamrosters.write(string_list(sharks))
+            teamrosters.write("Dragons \n")
+            teamrosters.write(string_list(dragons))
+            teamrosters.write("Raptors \n")
+            teamrosters.write(string_list(raptors))
+            print("File(s) written.")
 
-    # Generate lists of experienced vs inexperienced players.
-    # Use loop to evenly pop experienced players into 3 team lists.
-    # Use another loop to pop remainder of players into same 3 teams.
-    
-    # with open('teams.txt', 'a') as teamrosters ( now included in line opening csv file):
-        # fieldnames = ['get_these_from_roster']
-        # teamrosters.write(csvfile, fieldnames=fieldnames)
+    else:
+        print("Error: Uneven teams.")
+
+    # Extra credit goals: Write welcome letter text for each player.
+
+    # For my own idea of what would make this software IRL useful:    
+    # Include functionality that can:
+    #     1. Divide players into larger teams for different league types.
+    #     2. Allow coordinators to insert number of teams to divide players into.
+    #     3. Take player heights into account to keep teams evenly matched.
+    #     4. Return error message or something if teams cannot be evenly matched. 
+
 else:
     print("Due to tariffs, this file cannot be imported.")
