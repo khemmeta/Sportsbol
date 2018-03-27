f4b7196582e8d5cfe9b6c5f40e452fd0cde3dd08
Khem Myrick <khemmeta@gmail.com>
2018-03-27 12:55:22 -0700

    Updated league_builder.py
    Updating near final draft of extra credit ready soccer league project.  File writes final roster txt file, and welcome letters to each of the 18 players' guardians.

--- league_builder.py
+++ league_builder.py
@@ -7,9 +7,9 @@ advanced = []
 recruits = []
 
 def unpacker(pre_rosters):
+    """Unpack one player at a time into final team lists."""
     row_list = []
     while len(row_list) < 4:
-        """ Unpack one player at a time into final team lists. """
         row_list.append(pre_rosters[0]["Name"])        
         row_list.append(pre_rosters[0]["Height (inches)"])        
         row_list.append(pre_rosters[0]["Soccer Experience"])        
@@ -19,7 +19,7 @@ def unpacker(pre_rosters):
         return row_list
 
 def string_list(team):
-    """ Prepare team strings."""
+    """Prepare team strings for final roster."""
     player_string = ''
     for player in team:        
         del player[1]
@@ -31,14 +31,25 @@ def string_list(team):
     player_string += '\n'
     return player_string
         
-
+def acceptance_letters(team, team_string):
+    """Write acceptance letters to parents."""
+    for player in team:
+        file_name = str(player[0].replace(' ', '_'))
+        file_name += '.txt'
+        letter_string = """Dear {}, \n
+    Congratulations!  {} has been accepted into the {} family! 
+First practice session is on Friday at 5pm.  We recommend you arrive 15 minutes early.
+We look forward to seeing you on our fields.  Enjoy your year! \n
+    Sincerely,
+        Khem Myrick, League Coordinator""".format(player[3], player[0], team_string)
+        with open(file_name, 'w') as welcome_letter:
+            welcome_letter.write(letter_string)
+    print("{} acceptance letters written.".format(team_string))
+    
 if __name__ == "__main__":
-
-
-    with open('soccer_players.csv', newline='') as csvroster: 
-        # Get players from csv file and put into roster list.
+    # Nothing below will run when file is imported.
+    with open('soccer_players.csv', newline='') as csvroster:
         roster = list(csv.DictReader(csvroster, delimiter=','))
-        # We should be done with the csv file.  Deindenting steps below.
         
     for player in roster:
         # Seperate experienced players from beginners.
@@ -46,42 +57,39 @@ if __name__ == "__main__":
             advanced.append(player)
         else:
             recruits.append(player)
-            # This part of my code does what I want it to do!
+            # The above block of code was an early success in this process.
 
     while advanced or recruits:
-        """ Distribute players to actual teams. """
+        # Distribute players to actual teams.
         if advanced != []:
+            # Seperate advanced players 1 by 1 to each team until we run out.
             sharks.append(unpacker(advanced))
             dragons.append(unpacker(advanced))
             raptors.append(unpacker(advanced))
         elif recruits != []:
+            # After we run out of advanced players,
+            # seperate novice players 1 by 1 to each team until we run out.
             sharks.append(unpacker(recruits))
             dragons.append(unpacker(recruits))
             raptors.append(unpacker(recruits))
 
-
     if len(sharks) == len(raptors) and len(raptors) == len(dragons):
+        # If all teams have equal number of players, write acceptance letters.
+        acceptance_letters(sharks, 'Sharks')
+        acceptance_letters(raptors, 'Raptors')
+        acceptance_letters(dragons, 'Dragons')        
         with open('teams.txt', 'a') as teamrosters:
-            
+            # Write final roster.
             teamrosters.write("Sharks \n")
             teamrosters.write(string_list(sharks))
             teamrosters.write("Dragons \n")
             teamrosters.write(string_list(dragons))
             teamrosters.write("Raptors \n")
             teamrosters.write(string_list(raptors))
-            print("File(s) written.")
-
+            print("Final roster file written.")
     else:
         print("Error: Uneven teams.")
 
-    # Extra credit goals: Write welcome letter text for each player.
-
-    # For my own idea of what would make this software IRL useful:    
-    # Include functionality that can:
-    #     1. Divide players into larger teams for different league types.
-    #     2. Allow coordinators to insert number of teams to divide players into.
-    #     3. Take player heights into account to keep teams evenly matched.
-    #     4. Return error message or something if teams cannot be evenly matched. 
-
 else:
+    # Explicitly announce that code will not run as imported module.
     print("Due to tariffs, this file cannot be imported.")
